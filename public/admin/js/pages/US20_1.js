aac59dcf47c410a2d5f6d7cf86ef3193c10d8e67
longgane <100194921+longgane@users.noreply.github.com>
2022-03-16 11:46:24 +0700

    table

--- public/admin/js/pages/US20_1.js
+++ public/admin/js/pages/US20_1.js
@@ -1,61 +1,59 @@
-//-------------------- table 1--------------------------------------------------------------------------
+let state1 = {};
 
-let state = {};
+function pagination1(querySet1, page1, rows1) {
+    var trimStart1 = (page1 - 1) * rows1
+    var trimEnd1 = trimStart1 + rows1
 
-function pagination(querySet, page, rows) {
-    var trimStart = (page - 1) * rows
-    var trimEnd = trimStart + rows
+    var trimmedData1 = querySet1.slice(trimStart1, trimEnd1)
 
-    var trimmedData = querySet.slice(trimStart, trimEnd)
-
-    var pages = Math.round(querySet.length / rows);
+    var pages1 = Math.round(querySet1.length / rows1);
 
     return {
-        'querySet': trimmedData,
-        'pages': pages,
+        'querySet1': trimmedData1,
+        'pages1': pages1,
     }
 }
 
-function pageButtons1(pages) {
-    var wrapper = document.getElementById('pagination-wrapper1')
+function page1Buttons1(pages1) {
+    var wrapper1 = document.getElementById('pagination-wrapper1')
 
-    wrapper.innerHTML = ``
+    wrapper1.innerHTML = ``
 
-    var maxLeft = (state.page - Math.floor(state.window / 2))
-    var maxRight = (state.page + Math.floor(state.window / 2))
+    var maxLeft1 = (state1.page1 - Math.floor(state1.window1 / 2))
+    var maxRight1 = (state1.page1 + Math.floor(state1.window1 / 2))
 
-    if (maxLeft < 1) {
-        maxLeft = 1
-        maxRight = state.window
+    if (maxLeft1 < 1) {
+        maxLeft1 = 1
+        maxRight1 = state1.window1
     }
 
-    if (maxRight > pages) {
-        maxLeft = pages - (state.window - 1)
+    if (maxRight1 > pages1) {
+        maxLeft1 = pages1 - (state1.window1 - 1)
 
-        if (maxLeft < 1) {
-            maxLeft = 1
+        if (maxLeft1 < 1) {
+            maxLeft1 = 1
         }
-        maxRight = pages
+        maxRight1 = pages1
     }
 
-    for (var page = maxLeft; page <= maxRight; page++) {
-        wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`
+    for (var page1 = maxLeft1; page1 <= maxRight1; page1++) {
+        wrapper1.innerHTML += `<button value=${page1} class="page1 btn btn-sm btn-info">${page1}</button>`
     }
 
 
-    if (state.page != 1 && pages > 5) {
-        wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info">&#171; 初め</button>` + wrapper.innerHTML
+    if (state1.page1 != 1 && pages1 > 5) {
+        wrapper1.innerHTML = `<button value=${1} class="page1 btn btn-sm btn-info">&#171; 初め</button>` + wrapper1.innerHTML
     }
 
-    if (state.page != pages && pages > 5) {
-        wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info">最後 &#187;</button>`
+    if (state1.page1 != pages1 && pages1 > 5) {
+        wrapper1.innerHTML += `<button value=${pages1} class="page1 btn btn-sm btn-info">最後 &#187;</button>`
     }
 
     // Set color for active button.
-    $(`button[value="${state.page}"]`).addClass('active');
+    $(`button[value="${state1.page1}"]`).addClass('active');
 
-    $('.page').on('click', function() {
-        state.page = Number($(this).val())
+    $('.page1').on('click', function() {
+        state1.page1 = Number($(this).val())
         buildTable1()
     });
 }
@@ -67,11 +65,11 @@ function loadTableData1(conditions) {
                         <td colspan="8" style="padding: 0;  text-align: center;"><img style="width: 60px; height:  60px;" src="/assets/img/form/loading.svg" /></td>
                     </tr>
                     `);
-    state = {
-        'querySet': [],
-        'page': 1,
-        'rows': 2,
-        'window': 5,
+    state1 = {
+        'querySet1': [],
+        'page1': 1,
+        'rows1': 3,
+        'window1': 5,
     }
 
     // ------ DUMMY DATA ----------
@@ -85,7 +83,7 @@ function loadTableData1(conditions) {
 
     for (var i = 1; i <= 20; i++) {
         dummy_data.id = i;
-        state.querySet.push(dummy_data);
+        state1.querySet1.push(dummy_data);
     }
     // ------ ----------
 
@@ -100,7 +98,7 @@ function loadTableData1(conditions) {
     //         contentType: 'application/json',
     //     })
     //     .done(function(data) {
-    //         state.querySet = data.result;
+    //         state1.querySet1 = data.result;
 
     //         buildTable();
     //     })
@@ -112,8 +110,8 @@ function loadTableData1(conditions) {
 function buildTable1() {
     let items = '';
 
-    var data = pagination(state.querySet, state.page, state.rows);
-    var chunk = data.querySet;
+    var data = pagination1(state1.querySet1, state1.page1, state1.rows1);
+    var chunk = data.querySet1;
 
     chunk.forEach(function(data) {
         items += `
@@ -130,31 +128,10 @@ function buildTable1() {
     $("#table_data1").find("tr:gt(0)").remove();
     $('#table_data1').append(items);
 
-    pageButtons1(data.pages);
+    page1Buttons1(data.pages1);
 }
 
-function doSearch() {
-    let search_conditions = getDataFromForm('form');
-    loadTableData1(search_conditions);
-}
 
 $(document).ready(function() {
     loadTableData1();
-
-    /**
-     * Handle press enter key
-     */
-    $(document).on('keypress', function(e) {
-        if (e.which == 13) {
-            doSearch();
-        }
-    });
-
-    $('#reset_form_button').click(function() {
-        loadTableData1();
-    });
-
-    $('#search_button').click(function() {
-        doSearch();
-    });
 });
